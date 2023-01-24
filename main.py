7984a2522c61e3809f134e720f182a731ca173c8
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-01-24 09:04:51 -0500

    Wrote the main.py function to do the math. Finished! (to run again at a new CL, replace the number on line 7 in the while clause)

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-# initial commit
+# just literally wanted to do the math on Marvel Snap's Collectors Reserves,
+# to know which 'box' of 4 reserves I'm currently in...
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    start = 1006
+    # the collection level (CL1006) where reserves begin
+    while start < 2411:
+        # my collection is currently CL 2410
+        newlist = []
+        # instantiate a new list to replace the last one
+        for _ in range(4):
+            # every 4 reserves from 1006
+            newlist.append(start)
+            start += 4
+        print(newlist)
+
+"""
+Results: [1006, 1010, 1014, 1018] ... [2398, 2402, 2406, 2410]
+We can see from this that my CL2410 has just completed a "box" of 4 Collector's Reserves.
+"""
