87875947bc619978052efb12cf55997c7a49b8b6
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-01-25 12:05:38 -0500

    Updates including a revision of which "case" I'm on (started 4th at 2446)

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # just literally wanted to do the math on Marvel Snap's Collectors Reserves,
-# to know which 'box' of 4 reserves I'm currently in...
+# to know which 'pack' of 4 reserves/boxes I'm currently in...
 
 if __name__ == '__main__':
     start = 1006
     # the collection level (CL1006) where reserves begin
-    while start < 2411:
-        # my collection is currently CL 2410
+    while start < 2471:
+        # my collection is currently CL 2470
         newlist = []
         # instantiate a new list to replace the last one
         for _ in range(4):
@@ -15,6 +15,9 @@ if __name__ == '__main__':
         print(newlist)
 
 """
-Results: [1006, 1018, 1030, 1042] ... [2398, 2410, 2422, 2434]
-We can see from this that my CL2410 is 2 "packs" into a "box" of 4 Collector's Reserves.
+Results: [1006, 1018, 1030, 1042] ... [2446, 2458, 2470, 2482]
+We can see from this that my CL2470 is 3 "boxes" into a "pack" of 4 Collector's Reserves (box).
+To find out what "case" of 10 boxes you're on, on line 14 use "start += 480
+(12 CL's per box (Reserve), 4 per "pack", 10 packs per case aka 12*4*10 = 480)
+This code will still sort the results into lists of four, but that's not relevant for cases.
 """
