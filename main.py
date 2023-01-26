bf4d7bb6195614104eb579483efcdc80b82b209d
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-01-26 10:17:12 -0500

    Added a case-counting feature. Each case contains one Series 4 card, so ppl want to know!

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 if __name__ == '__main__':
     start = 1006
     # the collection level (CL1006) where reserves begin
+    casecount = 0
+    # a case is 10 "packs" of 4 "boxes" each (a "box" is 12 CL's with 1 Reserve)
     while start < 2543:
         # my collection is currently CL 2542
         newlist = []
@@ -13,12 +15,16 @@ if __name__ == '__main__':
             newlist.append(start)
             start += 12
             # a reserve (box) occurs every 12 CL
+        if (start - 1054) % 480 == 0:
+            # 1054 is very first CL of the second "pack" in case 1. [It makes the math work.]
+            casecount += 1
+            print(f'cracking open case {casecount}:')
         print(newlist)
 
 """
-Results: [1006, 1018, 1030, 1042] ... [2446, 2458, 2470, 2482]
-We can see from this that my CL2470 is 3 "boxes" into a "pack" of 4 Collector's Reserves (box).
-To find out what "case" of 10 boxes you're on, on line 14 use "start += 480
+Results: [1006, 1018, 1030, 1042] ... [2542, 2554, 2566, 2578]
+We can see from this that my CL2542 is 1 "box" into a "pack" of 4 Collector's Reserves (box).
+To find out what "case" of 10 boxes you're on, on line 17 use "start += 480"
 (12 CL's per box (Reserve), 4 per "pack", 10 packs per case aka 12*4*10 = 480)
 This code will still sort the results into lists of four, but that's not relevant for cases.
 """
