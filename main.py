f91248dcf6d5f6669e5578a61394201c1bdcf5fb
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-01-30 10:56:56 -0500

    - fixed the box_count math to accurately report which Reserve we're on - (took it out of the for loop to speed up computing (nominally))

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 if __name__ == '__main__':
     start = 1006
     # the collection level (CL1006) where reserves begin
-    CL = input("Enter your Collection Level as an integer:")
+    CL = int(input("Enter your Collection Level as an integer:"))
     box_count = 0
     pack_count = 1
     case_count = 0
     # start the counts at 0. A case is 10 "packs" of 4 "boxes" each
     # (a "box" is 12 CL's incl Boosters, Credits, and a Reserve)
-    while start < int(CL) + 1:
+    while start < CL + 1:
         # runs until it reaches your CL, then stops
         new_list = []
         # instantiate new list of boxes per loop
@@ -18,9 +18,8 @@ if __name__ == '__main__':
         # keeps track of which pack we're counting
         for _ in range(4):
             # every 4 reserves from 1006:
-            box_count += 1
-            # keeps track of which box we're on
             new_list.append(start)
+            # add the no. representing the reserve to the list
             start += 12
             # a reserve (box) occurs every 12 CL
         if (start - 1054) % 480 == 0:
@@ -28,7 +27,8 @@ if __name__ == '__main__':
             pack_count = 1
             # reset the pack count and start on the next case:
             case_count += 1
-            print(f'Cracking open CASE {case_count}:')
+            print(f'Cracking open Case {case_count}:')
         print(f"pack {pack_count}: ", new_list)
-    print(f"Your Collection is [printing out thru box {box_count}] of pack {pack_count} in case {case_count}")
+    box_count = ((CL - new_list[0]) // 12) + 1
+    print(f"Your Collection is on box {box_count} of pack {pack_count} in case {case_count}")
 
