4b4135fde632143dd7bb8ceb106f76bb31a33079
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-01-30 12:15:15 -0500

    - fixed the box_count math to accurately report which Reserve we're on - (took it out of the for loop to speed up computing (nominally))

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ if __name__ == '__main__':
         # runs until it reaches your CL, then stops
         new_list = []
         # instantiate new list of boxes per loop
-        box_count = 0
-        # resets the box count going into the next pack
         pack_count += 1
         # keeps track of which pack we're counting
         for _ in range(4):
@@ -30,5 +28,6 @@ if __name__ == '__main__':
             print(f'Cracking open Case {case_count}:')
         print(f"pack {pack_count}: ", new_list)
     box_count = ((CL - new_list[0]) // 12) + 1
+    # your CL minus the no. of the first reserve, with math to arrive at a 1-4
     print(f"Your Collection is on box {box_count} of pack {pack_count} in case {case_count}")
 
