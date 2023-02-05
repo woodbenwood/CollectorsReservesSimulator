e2d4182e9b4533c864084dfd1b3fe97e5dacaf48
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-02-05 15:22:14 -0500

    - enhancing the "stub" of the randomized option with actual random s3-5 draws - random_opening.py file added to house some of the more complex functions - and random_4_replit.py bc it doesn't have Fortuna

--- app/random_4_replit.py
+++ app/random_4_replit.py
@@ -1,4 +1,5 @@
-from random import random
+from random import random, choice
+from drs_dict import s3_set, s4_set, s5_set
 
 
 def s5chance(percent=.0025):
@@ -14,27 +15,40 @@ def twenty_five_percent_chance(percent=.25):
     return random() <= percent
 
 
+def roll_reserve():
+    if s5chance():
+        return choice(s5_set)
+    elif s4chance():
+        return choice(s4_set)
+    else:
+        return choice(s3_set)
+
+
 if __name__ == '__main__':
-    print('s5\'s:')
-    count = 0
-    for _ in range(100000):
-        roll_s5 = s5chance()
-        if roll_s5:
-            count += 1
-    print(count)
+    for i in range(10):
+        rolling = roll_reserve()
+        print(rolling())
 
-    count = 0
-    print('s4\'s:')
-    for _ in range(100000):
-        roll_s4 = s4chance()
-        if roll_s4:
-            count += 1
-    print(count)
-
-    count = 0
-    print('25\'s:')
-    for _ in range(100000):
-        roll_25 = twenty_five_percent_chance()
-        if roll_25:
-            count += 1
-    print(count)
+    # print('s5\'s:')
+    # count = 0
+    # for _ in range(100000):
+    #     roll_s5 = s5chance()
+    #     if roll_s5:
+    #         count += 1
+    # print(count)
+    #
+    # count = 0
+    # print('s4\'s:')
+    # for _ in range(100000):
+    #     roll_s4 = s4chance()
+    #     if roll_s4:
+    #         count += 1
+    # print(count)
+    #
+    # count = 0
+    # print('25\'s:')
+    # for _ in range(100000):
+    #     roll_25 = twenty_five_percent_chance()
+    #     if roll_25:
+    #         count += 1
+    # print(count)
