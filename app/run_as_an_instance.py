8c343856b2765f60f48c7c6829d18b8c29321bad
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-02-16 15:33:59 -0500

    - Created Run Class for best refactor yet - removing unused imports from run_as_an_instance.py

--- app/run_as_an_instance.py
+++ app/run_as_an_instance.py
@@ -1,5 +1,6 @@
 from app.drs_dict import dr_CL
 from app.functions import run_dr, run_random, run_basic
+# importing and instantiating the Run class isn't even necessary, can just run its class functions:
 
 choice = input("Enter your Collection Level or 'random':")
 
