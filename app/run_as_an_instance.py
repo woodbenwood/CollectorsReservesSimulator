c9a980e145fdc738ee7b27664f8a0b13cad2d568
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-02-16 15:20:30 -0500

    - Created Run Class for best refactor yet - removing unused imports from run_as_an_instance.py

--- app/run_as_an_instance.py
+++ app/run_as_an_instance.py
@@ -1,9 +1,6 @@
-from app.drs_dict import dr_dict, dr_CL, s3_set, s4_set, s5_set
-from app.random_4_replit import s4chance, s5chance, pick_a_pack, roll_cache, roll_reserve, pick_a_box, \
-     cache_pull, s4_timer, timer_card
-from app.functions import run_dr, run_random, run_basic
+from app.drs_dict import dr_CL
+from app.functions import run_random, run_basic
 from app.run_as_a_class import Run
-
 
 choice = input("Enter your Collection Level or 'random':")
 run_as_an_instance = Run()
