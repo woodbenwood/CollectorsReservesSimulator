0d7029c0fd6071d1fc292a99165ff8c536e01e41
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-06-17 19:51:02 -0400

    - updates following June 2023 patch

--- app/main.py
+++ app/main.py
@@ -56,7 +56,8 @@ while start < CL + 1:
     for i in range(4):
         # every 4 Reserves from 1006:
         if choice == "Dr":
-            if start in dr_dict.keys():
+            if start in dr_dict.keys() and dr_dict.values(start).isalpha():
+                # when this works, it will rule out the dict entries containing integers (ala "200 tokens")
                 memory = start
                 new_list.append(dr_dict[start])
                 # print to the list of Reserves the card opened there
