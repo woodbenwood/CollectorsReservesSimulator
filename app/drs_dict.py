7aeac077ff66d5d48b3fedd113e9359f3d6a7860
Ben Wood <97207372+woodbenwood@users.noreply.github.com>
2023-07-19 12:11:14 -0400

    - updates following July 2023 patch

--- app/drs_dict.py
+++ app/drs_dict.py
@@ -1,4 +1,4 @@
-dr_CL = 7900
+dr_CL = 8200
 
 s3_set = ['Absorbing Man', 'Adam Warlock', 'Aero', 'Agatha Harkness', 'Agent Coulson', 'Arnim Zola', 'Attuma',
           'Baron Mordo', 'Bast', 'Black Panther', 'Beast', 'Black Bolt', 'Black Cat', 'Black Widow', 'Brood',
